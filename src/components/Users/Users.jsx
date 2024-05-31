a78bc202c4bd11d78c64c237b1863a699563fe20
Ptomainchik <fantasticdenis@mail.ru>
2024-05-31 05:06:26 +0300

    mapD, Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,48 @@
 import React from "react";
-import classes from "./Users.module.css"
-import axios from "axios";
-import userPhoto from "./../assets/images/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+import classes from "./Users.module.css";
+import userPhoto from "./../assets/images/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png";
 
-class Users extends React.Component{
-    componentDidMount(){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            this.props.setUsers(response.data.items)
-        })}
+let Users = (props) => {
+  
+    let pagesCount = Math.ceil (props.totalUsersCount / props.pageSize);
 
-    render() {
-        return <div>
-        {this.props.users.map(u => <div key= {u.id}>
-            <span>
-                <div>
-                    <img src={u.photos.small != null ? u.photos.small: userPhoto} className={classes.photo} alt="Slon"/>
-                </div>
-                <div>
-                    { u.followed ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button> 
-                    : <button onClick={ () => {this.props.follow(u.id)}}>Follow</button>}
-                    
-                </div>
-            </span>
-            <span>
-              <span>
-               <div>{u.name}</div>
-               <div>{u.status}</div>
-              </span>
-              <span>
-                <div>{'u.location.country'}</div>
-                <div>{'u.location.city'}</div>
-              </span>
-            </span>
-        </div>)}
-    </div>
+    let pages = [];
+    for (let i=1; i <= pagesCount; i++) {
+        pages.push(i);
     }
-}
 
+ return <div>
+    <div>
+        {pages.map(p => {
+           return <span  className={props.currentPage === p && classes.selectedPage}
+            onClick={(e) => {props.onPageChanged(p)}}>{p}</span>
+        })}
+        
+    </div>
+{props.users.map(u => <div key= {u.id}>
+    <span>
+        <div>
+            <img src={u.photos.small != null ? u.photos.small: userPhoto} className={classes.photo} alt="Slon"/>
+        </div>
+        <div>
+            { u.followed ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> 
+            : <button onClick={ () => {props.follow(u.id)}}>Follow</button>}
+            
+        </div>
+    </span>
+    <span>
+      <span>
+       <div>{u.name}</div>
+       <div>{u.status}</div>
+      </span>
+      <span>
+        <div>{'u.location.country'}</div>
+        <div>{'u.location.city'}</div>
+      </span>
+    </span>
+</div>)}
+</div>
+}
 
 
 export default Users;
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import preloader from "../../assets/images/ZKZg.gif";
+
+
+let Preloader = () => {
+    return ( <img src={preloader} alt="preloader"/>)
+}
+
+export default Preloader;
